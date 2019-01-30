0532f2704f85a0ad9e6bfa1e02bc04a5a56964f0
declarative-spooling-5152 <kerneltux0@gmail.com>
2019-01-30 16:34:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  array = [1,2,3]
-  sq_array = []
-  array.each do |number|
-    sq_array << number**2
+  data = [1,2,3]
+  array = []
+  data.each do |number|
+    array << number**2
   end
 end
